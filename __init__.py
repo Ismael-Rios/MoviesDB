6644a4faf1586bb4d54fada83504575735f1dc9a
Ismael Rios <ismaelvrios1993@gmail.com>
2021-07-19 14:50:22 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-print('Hello World!')
+import sys
+
+sys.path.append('modules/')
+#sys.path.append('resources/')
+
+def main():
+    userName()
+    
+    
+if __name__ == "__main__":
+    main()
