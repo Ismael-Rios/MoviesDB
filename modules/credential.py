44152fb0b2f1644c96c555a607bd98d4537d4a6b
Ismael Rios <ismaelvrios1993@gmail.com>
2021-07-29 17:47:45 -0300

    Update and rename userName.py to credential.py

---
+++ modules/credential.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+def md5(uName, uPass):
+    userName = uName
+    userPass = uPass
+    
+    md5 = userName + userPass
+
+    return(md5)
--- modules/userName.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def userName():
-    print('Enter username:')
-    user = input()
-
-    return(user)
