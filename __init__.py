a89223d2433c74e857f8d1566060e9b0ab030f9d
Ismael Rios <ismaelvrios1993@gmail.com>
2021-07-30 14:10:07 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,71 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import sys
-import tkinter as tk
-
-from modules.credential import md5
+from modules.login import *
 
 def main():
     login()
-    
-def login():
-    def send():
-        userName = userEntry.get()
-        userPass = passEntry.get()
-        
-        hash = md5(userName, userPass)
-        print(hash)
-        
-    def close():
-        window.destroy()
-
-    window = tk.Tk()
-    
-    loginFrame = tk.Frame()
-    loginFrame.pack()
-    
-    userLabel = tk.Label(
-        master=loginFrame, 
-        text="Username"
-    )
-    userLabel.pack()
-    
-    userEntry = tk.Entry(
-        master=loginFrame
-    )
-    userEntry.pack()
-    
-    passLabel = tk.Label(
-        master=loginFrame, 
-        text="Password"
-    )
-    passLabel.pack()
-    
-    passEntry = tk.Entry(
-        master=loginFrame
-    )
-    passEntry.pack()
-    
-    loginButton = tk.Button(
-        master=loginFrame,
-        text="Login",
-        width=7,
-        height=1,
-        command=send,
-    )
-    loginButton.pack()
-    
-    closeButton = tk.Button(
-        master=loginFrame,
-        text="Close",
-        width=7,
-        height=1,
-        command=close,
-    )
-    closeButton.pack()
-    
-    window.mainloop()
-    return()
     
 if __name__ == "__main__":
     main()
